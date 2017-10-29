5a7c528e4d78003bc5e08ded4ab50ebf1c7aa1a7
kemikalgeneral <kemikalgeneral@gmail.com>
2017-10-29 19:39:09 +0000

    Added ActionBar titles for CreateDraw and DrawDetail

--- app/src/main/java/com/example/kemik/bonusball/CreateDraw.java
+++ app/src/main/java/com/example/kemik/bonusball/CreateDraw.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,9 @@ public class CreateDraw extends AppCompatActivity
 
         // Find all views
         findViews();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Create Draw...");
 
         // Capture drawName and drawValue, and call to create a new draw.
         // When finished, go to MainActivity on create button click.
--- app/src/main/java/com/example/kemik/bonusball/DrawDetail.java
+++ app/src/main/java/com/example/kemik/bonusball/DrawDetail.java
@@ -2,6 +2,7 @@ package com.example.kemik.bonusball;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -26,6 +27,9 @@ public class DrawDetail extends AppCompatActivity {
 
         // Find all views
         findViews();
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Draw Details...");
 
         // Instantiate a new DBHelper class
         DBHelper db = new DBHelper(this);
