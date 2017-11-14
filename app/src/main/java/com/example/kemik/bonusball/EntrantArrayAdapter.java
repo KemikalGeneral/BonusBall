7e6f5eec76bf1e2aae4f9d0f6663ab18f888e3d4
kemikalgeneral <kemikalgeneral@gmail.com>
2017-11-14 19:23:40 +0000

    Entrant chosen line numbers now change to reflect the payment status for ease of use.

--- app/src/main/java/com/example/kemik/bonusball/EntrantArrayAdapter.java
+++ app/src/main/java/com/example/kemik/bonusball/EntrantArrayAdapter.java
@@ -3,6 +3,7 @@ package com.example.kemik.bonusball;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -90,6 +91,10 @@ class EntrantArrayAdapter extends ArrayAdapter<Entrant> {
                 return false;
             }
         });
+
+        // Change colour of line numbers to reflect the payment status (green=paid, red=unpaid)
+        setNumberColourOnPaymentChange(entrant, tv_slotNumber);
+
         return convertView;
     }
 
@@ -136,6 +141,20 @@ class EntrantArrayAdapter extends ArrayAdapter<Entrant> {
         db.changePaymentStatus(chosenNumber, drawId, status);
 
         refreshThisActivity();
+    }
+
+    /**
+     * Change the colour of the line number to reflect the payment status (green=paid, red=unpaid)
+     *
+     * @param entrant
+     * @param tv_slotNumber
+     */
+    private void setNumberColourOnPaymentChange(Entrant entrant, TextView tv_slotNumber) {
+        if (entrant.getPaymentStatus() == null || entrant.getPaymentStatus().equals("unpaid")) {
+            tv_slotNumber.setTextColor(Color.RED);
+        } else {
+            tv_slotNumber.setTextColor(Color.GREEN);
+        }
     }
 
     /**
